81820801aafdd3e1ec043e30c8004123ce350bee
survivor <ramis.m@genee.coach>
2025-11-24 01:38:32 +0500

    work

--- slidingwindowmaximum.py
+++ slidingwindowmaximum.py
@@ -2,5 +2,44 @@
 # there is a sliding window of size k which is moving from the very left of the array to the very right.
 # You can only see the k numbers in the window. Each time the sliding window moves right by one position.
 # Return the max sliding window.
+from collections import deque
 
 
+# def slidingwindowmax(nums: list, k: int) -> list:
+#     n_len = len(nums)
+#     if n_len <= 0 or k <= 0:
+#         return [0]
+#     answer = []
+#     queue = deque()
+#
+#     for i in range(n_len):
+#         if queue:
+#             if queue[0] < i - k:
+#                 queue.popleft()
+#
+#         while queue and nums[queue[-1]] <= nums[i]:
+#             queue.pop()
+#
+#         queue.append(i)
+#         if i >= k - 1:
+#             answer.append(nums[queue[0]])
+#
+#     return answer
+
+
+def slidingwindowmax(nums: list, k: int) -> list:
+    answer = []
+    queue = deque()
+
+    for i, n in enumerate(nums):
+        while queue and queue[-1] < n:
+            queue.pop()
+        queue.append(n)
+        if i >= k and nums[i - k] == queue[0]:
+            queue.popleft()
+        if i >= k - 1:
+            answer.append(queue[0])
+    return answer
+
+
+print(slidingwindowmax([1, 3, -1, -3, 5, 3, 6, 7], 3))
