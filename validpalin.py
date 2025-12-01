ccd8eff0655cc6dbb17bfc21f472bab23e98777a
survivor <ramismajeed@gmail.com>
2025-12-01 17:38:55 +0500

    Create validpalin.py

---
+++ validpalin.py
@@ -0,0 +1,15 @@
+
+def validpalindrome(s:str)->bool:
+
+    s = [k for k in s if k.isalnum()]
+    s = "".join(s)
+    s = s.lower()
+    end = len(s) - 1
+    for i in range(len(s)):
+        if s[i]!= s[end]:
+            return False
+        end-=1
+
+    return True
+
+print(validpalindrome("0P"))
